67cebffba6f0092f61e71259ff1979d5bebb3ac9
Jeffrey <j.lo128456@gmail.com>
2024-06-23 15:04:53 +1000

    completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let textElement = document.getElementById('text');
+    textElement.textContent = 'This is really cool!';
+
+    console.log("The DOM has loaded");
+  });
+  console.log("The DOM has not loaded Yet")
